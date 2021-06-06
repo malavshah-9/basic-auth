22150841a067d68790abe796424704784d71cc14
Malav Shah <malav.shah@smartsensesolutions.com>
2021-06-06 20:19:27 +0530

    refactor: add validator

--- src/components/address/v1/address.routes.ts
+++ src/components/address/v1/address.routes.ts
@@ -1,11 +1,28 @@
 import { Router } from 'express';
 import addressController from './address.controller';
+import validator from './address.validator';
 
 const AddressRouter = Router();
 
-AddressRouter.post('/address', addressController.create);
-AddressRouter.get('/address/:userId', addressController.getByUserId);
-AddressRouter.put('/address/:addressId', addressController.updateAddress);
-AddressRouter.delete('/address/:addressId', addressController.deleteAddress);
+AddressRouter.post(
+  '/address',
+  validator.validateCreateAddress,
+  addressController.create
+);
+AddressRouter.get(
+  '/address/:userId',
+  validator.validateGetAddressByUserId,
+  addressController.getByUserId
+);
+AddressRouter.put(
+  '/address/:addressId',
+  validator.validateUpdateAddress,
+  addressController.updateAddress
+);
+AddressRouter.delete(
+  '/address/:addressId',
+  validator.validateDeleteAddress,
+  addressController.deleteAddress
+);
 
 export default AddressRouter;
